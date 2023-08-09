66eefbbdde7b7ad2ce8cd892dcfde43cead67a83
HaowenWeiJohn <hw2892@columbia.edu>
2023-08-10 06:50:34 +0800

    aoi augmentation

--- Assets/Scripts/Preset/ExperimentPreset.cs
+++ Assets/Scripts/Preset/ExperimentPreset.cs
@@ -76,7 +76,7 @@ public static class ExperimentPreset
         List<Presets.ExperimentState> ExperimentStates = new List<Presets.ExperimentState> { };
 
         ExperimentStates = ExperimentStates.Concat(Presets.PracticeStartBlock).ToList();
-        ExperimentStates = ExperimentStates.Concat(Presets.NoAOIAugmentationBlockWithInstructionBlock).ToList();
+        ExperimentStates = ExperimentStates.Concat(Presets.NoAOIAugmentationBlock).ToList();
         ExperimentStates = ExperimentStates.Concat(Presets.StaticAOIAugmentationBlockWithInstructionBlock).ToList();
         ExperimentStates = ExperimentStates.Concat(Presets.InteractiveAOIAugmentationBlockWithInstructionBlock).ToList();
 
